9e3a555649bab24f0f52cef3de833a9884401e73
zxllo12 <poapog123@naver.com>
2024-07-18 16:46:06 +0900

    [INIT} 최초 커밋
    최초 커밋

---
+++ gittest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace gittest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, GitTTTTTTTTTT");
+        }
+    }
+}
